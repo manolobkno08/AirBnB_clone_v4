9bbe2e954a59654b66057d4d4f1fa81c1e8f3697
manolobkno08 <manologm10@hotmail.com>
2022-02-10 17:22:52 -0500

    ADD: add method ajax to print places

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -32,3 +32,18 @@ $('document').ready(function () {
 		$('.amenities h4').text(ProcessOutput((newList.join(', ')), 30));
 	});
 });
+
+// POST REQUEST 4
+$.ajax({
+	type: 'POST',
+	url: `http://${window.location.hostname}:5001/api/v1/places_search`,
+	data: '{}',
+	dataType: 'json',
+	contentType: 'application/json',
+	success: function (data) {
+		for (let i = 0; i < data.length; i++) {
+			let place = data[i];
+			$('.places ').append('<article><h2>' + place.name + '</h2><div class="price_by_night"><p>$' + place.price_by_night + '</p></div><div class="information"><div class="max_guest"><div class="guest_image"></div><p>' + place.max_guest + '</p></div><div class="number_rooms"><div class="bed_image"></div><p>' + place.number_rooms + '</p></div><div class="number_bathrooms"><div class="bath_image"></div><p>' + place.number_bathrooms + '</p></div></div><div class="description"><p>' + place.description + '</p></div></article>');
+		}
+	}
+});
