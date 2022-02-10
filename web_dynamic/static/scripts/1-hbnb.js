e9e64966d732a69aab0648a562161854a0ed86f1
manolobkno08 <manologm10@hotmail.com>
2022-02-09 19:52:09 -0500

    task 2 fixed

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,8 +1,14 @@
 $(function () {
 	let newList = [];
-	$("input[type='checkbox']").on('change', function () {
+	$("input[type='checkbox']").click(function () {
 		if ($(this).is(":checked")) {
 			newList.push($(this).attr("data-name"));
+		}
+		else if (!$(this).is(":checked")) {
+			let toRemove = $(this).attr("data-name");
+			newList = newList.filter(function (item) {
+				return item !== toRemove;
+			});
 		}
 		$('.amenities h4').text(newList.join(', '));
 	});
