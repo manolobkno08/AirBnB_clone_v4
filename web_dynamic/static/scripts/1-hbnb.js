931c52e66e5b8813c1d7c4efd3ecbfcce004083b
JuanSebastianGB <3393@holbertonschool.com>
2022-02-10 05:05:10 +0000

    UPD: Implementing task 3 API status.

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,21 +1,20 @@
 $(function () {
-	let newList = [];
-	$("input[type='checkbox']").click(function () {
-		if ($(this).is(":checked")) {
-			newList.push($(this).attr("data-name"));
-		}
-		else if (!$(this).is(":checked")) {
-			let toRemove = $(this).attr("data-name");
-			newList = newList.filter(function (item) {
-				return item !== toRemove;
-			});
-		}
-		$('.amenities h4').text(newList.join(', '));
-		// if (newList.length <= 2) {
-		// 	$('.amenities h4').text(newList.join(', '));
-		// }
-		// else if (newList.length > 2) {
-		// 	$('.amenities h4').text(newList.join(', ' + '...'));
-		// }
-	});
+  let newList = [];
+  $("input[type='checkbox']").click(function () {
+    if ($(this).is(':checked')) {
+      newList.push($(this).attr('data-name'));
+    } else if (!$(this).is(':checked')) {
+      const toRemove = $(this).attr('data-name');
+      newList = newList.filter(function (item) {
+        return item !== toRemove;
+      });
+    }
+    $('.amenities h4').text(newList.join(', '));
+    // if (newList.length <= 2) {
+    // 	$('.amenities h4').text(newList.join(', '));
+    // }
+    // else if (newList.length > 2) {
+    // 	$('.amenities h4').text(newList.join(', ' + '...'));
+    // }
+  });
 });
---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,24 @@
+const CheckStatus = async () => {
+  // Checking status of server in port 5000
+  const url = `http://${window.location.hostname}:5000/api/v1/status`;
+  await $.get(url, (data) => {
+    if (data.status === 'OK') { $('div#api_status').addClass('available'); } else { $('div#api_status').removeClass('available'); }
+  });
+};
+
+$('document').ready(function () {
+  CheckStatus();
+  let newList = [];
+  $("input[type='checkbox']").click(() => {
+    // Catching click event for checkbox
+    if ($(this).is(':checked')) {
+      newList.push($(this).attr('data-name'));
+    } else if (!$(this).is(':checked')) {
+      const toRemove = $(this).attr('data-name');
+      newList = newList.filter((item) => {
+        return item !== toRemove;
+      });
+    }
+    $('.amenities h4').text(newList.join(', '));
+  });
+});
