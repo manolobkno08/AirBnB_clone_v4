e9a43f32fc2b84ff0c471948044728a20246653e
JuanSebastianGB <3393@holbertonschool.com>
2022-02-12 02:35:51 +0000

    DEL: Deleting unused variable

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -53,7 +53,6 @@ async function HandleFilterPlacesByAmenities(AmenitiesDictionary = '') {
 async function DrawFilteredPlaces(AmenitiesDictionary = '') {
   // Print filtered places in the html tag
   const places = await GetFilteredPlaces(AmenitiesDictionary);
-  let ToPrintUser = '';
   $('.places').html('');
   places.map(async (place, i) => {
     const user = await GetUser(place.user_id);
